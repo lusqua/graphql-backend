4457598b4e6cf9bad82c8a33535d85877aa8d409
lusqua <lucasaguiar.floripa@gmail.com>
2024-06-28 10:27:08 -0300

    feat. remove extra console

--- src/modules/users/createUser.ts
+++ src/modules/users/createUser.ts
@@ -4,8 +4,6 @@ export const createUser = async (name: string) => {
   const testDb = client.db("test");
   const users = testDb.collection("users");
 
-  console.log("la vem");
-
   const result = await users.insertOne({ name });
 
   return await users.findOne({ _id: result.insertedId });
